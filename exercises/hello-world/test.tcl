4b6b4994408f480482c2b915eb09949845b245e5
Simon Shine <shreddedglory@gmail.com>
2019-08-08 14:31:13 +0200

    Make 'hello-world' and 'two-fer' fail fast.
    As a follow-up to the TDD proposal of making tests fail fast in #11,
    make the currently implemented exercises fail fast with this code.

--- exercises/hello-world/test.tcl
+++ exercises/hello-world/test.tcl
@@ -4,6 +4,14 @@ package require tcltest
 namespace import ::tcltest::*
 source "hello-world.tcl"
 
+proc test args {
+    if {$::tcltest::numTests(Failed) > 0} {
+        puts "**** Exit on first failure"
+        exit 1
+    }
+    uplevel [list ::tcltest::test {*}$args]
+}
+
 if {$::argv0 eq [info script]} {
     test hello_Hello {
         Test: [hello] == "Hello, World!"
--- exercises/two-fer/test.tcl
+++ exercises/two-fer/test.tcl
@@ -4,6 +4,14 @@ package require tcltest
 namespace import ::tcltest::*
 source "two-fer.tcl"
 
+proc test args {
+    if {$::tcltest::numTests(Failed) > 0} {
+        puts "**** Exit on first failure"
+        exit 1
+    }
+    uplevel [list ::tcltest::test {*}$args]
+}
+
 if {$::argv0 eq [info script]} {
 
     test two-fer-1 {
