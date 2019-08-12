e4372319f8e75f181c6f5052c35dda693b6a0b2f
Simon Shine <shreddedglory@gmail.com>
2019-08-12 10:11:35 +0200

    Make 'hello-world' and 'two-fer' fail fast. (#14)
    * Make 'hello-world' and 'two-fer' fail fast.
    
    As a follow-up to the TDD proposal of making tests fail fast in #11,
    make the currently implemented exercises fail fast with this code.
    
    Make failing fast the default:
    
        $ tclsh test.tcl
        [... fails fast ...]
    
    But make it possible to run all tests:
    
        $ RUN_ALL=1 tclsh test.tcl
        [... runs all tests ...]
    
    In both cases, print the bottom header, marking tests that have not been
    run as skipped in cast of fast failure. We make sure that tests don't exit
    prematurely by calling [exit 1], if necessary, after cleanup.

--- exercises/hello-world/test.tcl
+++ exercises/hello-world/test.tcl
@@ -4,6 +4,31 @@ package require tcltest
 namespace import ::tcltest::*
 source "hello-world.tcl"
 
+proc fail_fast {} {
+    return [expr {
+        ![info exists ::env(RUN_ALL)]
+        || [string is boolean -strict $::env(RUN_ALL)]
+        && !$::env(RUN_ALL)
+    }]
+}
+
+proc failed {} {
+    return [expr {$::tcltest::numTests(Failed) > 0}]
+}
+
+if {[fail_fast]} {
+    proc test args {
+        if {[failed]} {::tcltest::configure -skip *}
+        uplevel [list ::tcltest::test {*}$args]
+    }
+}
+
+proc cleanupTests {} {
+    set failed [failed]
+    uplevel 1 ::tcltest::cleanupTests
+    if {$failed} {exit 1}
+}
+
 if {$::argv0 eq [info script]} {
 
     test hello-1 {
