6ac0b42b95362a7812741d8901418bb02cadb6b5
Glenn Jackman <jackman@pythian.com>
2019-08-12 15:11:58 -0400

    Error handling: Create exercise
    Another core exercise.

---
+++ exercises/error-handling/.meta/hints.md
@@ -0,0 +1,4 @@
+## Resources
+
+The Tcl Wiki entry on [Errors management](https://wiki.tcl-lang.org/page/Errors+management)
+is useful for this exercise.
---
+++ exercises/error-handling/error-handling.tcl
@@ -0,0 +1,7 @@
+proc custom_error_message {message} {
+    throw {NOT_IMPLEMENTED} "You must write this procedure."
+}
+
+proc handle_error {script} {
+    throw {NOT_IMPLEMENTED} "You must write this procedure."
+}
