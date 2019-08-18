efa506df55f4119098d408bfb266011b25fd2ac9
Glenn Jackman <jackman@pythian.com>
2019-08-18 13:08:53 -0400

    Create exercise: resistor-color-duo

---
+++ exercises/resistor-color-duo/example.tcl
@@ -0,0 +1,19 @@
+namespace eval ::resistorColor {
+    variable colors {
+        black brown red orange yellow green blue violet grey white
+    }
+
+    proc colorCode {color} {
+        variable colors
+        set idx [lsearch -exact $colors $color]
+        if {$idx == -1} {
+            error "Invalid color: $color"
+        }
+        return $idx
+    }
+
+    proc value {first second args} {
+        set codes [lmap c [list $first $second] {colorCode $c}]
+        return [join $codes ""]
+    }
+}
---
+++ exercises/resistor-color-duo/resistor-color-duo.tcl
@@ -0,0 +1,5 @@
+namespace eval ::resistorColor {
+    proc value {} {
+        throw NOT_IMPLEMENTED "Implement this procedure."
+    }
+}
