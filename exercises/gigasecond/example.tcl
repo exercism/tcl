9f8a3510de66e698d94b64c95506842f9c976914
Glenn Jackman <jackman@pythian.com>
2019-08-18 12:47:35 -0400

    Gigasecond: create exercise

---
+++ exercises/gigasecond/example.tcl
@@ -0,0 +1,16 @@
+proc addGigasecond {datetime} {
+    # scan/format times in UTC to avoid DST offsets.
+    # date time parsing: try most specific format first
+    try {
+        set time [clock scan $datetime -format {%Y-%m-%dT%H:%M:%S} -gmt yes]
+    } on error {} {
+        try {
+            set time [clock scan $datetime -format {%Y-%m-%d} -gmt yes]
+        } on error {} {
+            set time [clock scan $datetime -gmt yes]
+        }
+    }
+
+    set future [clock add $time 1000000000 seconds]
+    return [clock format $future -format {%Y-%m-%dT%H:%M:%S} -gmt yes]
+}
