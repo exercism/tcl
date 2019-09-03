5521970516db2f3f7a1f278c98dbf122e1e04dd1
Glenn Jackman <jackman@pythian.com>
2019-09-03 11:27:24 -0400

    Refactoring part 2
    - only store parsed data as rows
    - no duplication of data
    - walk the rows to return requested column

--- exercises/matrix/example.tcl
+++ exercises/matrix/example.tcl
@@ -1,40 +1,17 @@
 oo::class create Matrix {
     constructor {inputString} {
-        variable input  $inputString
-        variable parsed false
         variable rows
-        variable columns
-    }
-
-    method ParseInput {} {
-        my variable rows columns input parsed
-
-        set rows [lmap line [split $input \n] {split $line}]
-
-        set columns [lrepeat [llength [lindex $rows 0]] {}]
-        for {set r 0} {$r < [llength $rows]} {incr r} {
-            for {set c 0} {$c < [llength $columns]} {incr c} {
-                lset columns $c $r [lindex $rows $r $c]
-            }
-        }
-
-        set parsed true
+        set rows [lmap line [split $inputString \n] {split $line}]
     }
 
     method row {n} {
-        my variable parsed rows
-        if {!$parsed} {
-            my ParseInput
-        }
+        my variable rows
         return [lindex $rows $n-1]
     }
 
     method column {n} {
-        my variable parsed columns
-        if {!$parsed} {
-            my ParseInput
-        }
-        return [lindex $columns $n-1]
+        my variable rows
+        return [lmap row $rows {lindex $row $n-1}]
     }
 }
 
