98bd212b3d6bf578ed479927e9c1e316ec1bc7c9
Glenn Jackman <jackman@pythian.com>
2019-09-11 10:11:30 -0400

    review feedback

--- exercises/word-count/example.tcl
+++ exercises/word-count/example.tcl
@@ -1,15 +1,8 @@
 proc countWords {sentence} {
-
-    # A "word" is"
-    # 1. A _number_ composed of one or more ASCII digits (ie "0" or "1234") OR
-    # 2. A _simple word_ composed of one or more ASCII letters (ie "a" or "they") OR
-    # 3. A _contraction_ of two _simple words_ joined by a single apostrophe (ie "it's" or "they're")
-
     set re {
         \m (?:
-            \d+
-            |
-            [[:alpha:]]+ (?: ' [[:alpha:]]+ )?
+            \d+ |                               # digits, or
+            [[:alpha:]]+ (?: ' [[:alpha:]]+ )?  # words, contractions
         ) \M
     }
 
--- exercises/word-count/word-count.tcl
+++ exercises/word-count/word-count.tcl
@@ -1,3 +1,5 @@
 proc countWords {sentence} {
+    set counts [dict create]
     throw NOT_IMPLEMENTED "Implement this procedure."
+    return $counts
 }
