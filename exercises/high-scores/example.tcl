c4b7a38ebdf3c21f33b35e757327bcdf9a4bf5c3
Glenn Jackman <glenn.jackman@gmail.com>
2019-09-30 14:03:53 -0400

    update method for declaring instance variables

--- exercises/high-scores/example.tcl
+++ exercises/high-scores/example.tcl
@@ -1,34 +1,31 @@
 #! tclsh
 
 oo::class create HighScores {
+    variable scores
+
     constructor {} {
-        variable scores [list]
+        set scores {}
     }
 
     method addScores {args} {
-        my variable scores
         lappend scores {*}$args
     }
 
     method scores {} {
-        my variable scores
         return $scores
     }
 
     method latest {} {
-        my variable scores
         return [lindex $scores end]
     }
     
     method personalBest {} {
-        my variable scores
         if {[llength $scores] > 0} {
             ::tcl::mathfunc::max {*}$scores
         }
     }
 
     method topThree {} {
-        my variable scores
         lrange [lsort -integer -decreasing $scores] 0 2
     }
 }
