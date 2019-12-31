0183a488a2d5e5ab61245c96dc06f3b10f3f5e5a
Glenn Jackman <glenn.jackman@gmail.com>
2019-12-30 19:28:02 -0500

    Space-age: create exercise (#61)

---
+++ exercises/space-age/example.tcl
@@ -0,0 +1,20 @@
+dict for {planet orbit} {
+    Mercury   0.2408467
+    Venus     0.61519726
+    Earth     1.0
+    Mars      1.8808158
+    Jupiter  11.862615
+    Saturn   29.447498
+    Uranus   84.016846
+    Neptune 164.79132
+} {
+    set secondsPerPlanetYear [expr {$orbit * 31557600}]
+
+    # Because we're defining the proc body with double quotes, we have
+    # to escape the command substitution and variable expansions 
+    # to delay evaluation until the procedure is called.
+
+    proc "on$planet" {ageInSeconds} "
+        return \[expr {1.0 * \$ageInSeconds / $secondsPerPlanetYear}]
+    "
+}
---
+++ exercises/space-age/space-age.tcl
@@ -0,0 +1,10 @@
+proc onEarth {} {error "Implemenet this procedure."}
+proc onMercury {} {error "Implemenet this procedure."}
+proc onVenus {} {error "Implemenet this procedure."}
+proc onMars {} {error "Implemenet this procedure."}
+proc onJupiter {} {error "Implemenet this procedure."}
+proc onSaturn {} {error "Implemenet this procedure."}
+proc onUranus {} {error "Implemenet this procedure."}
+proc onNeptune {} {error "Implemenet this procedure."}
+
+# extra credit: generate the procedures programmatically
