c174b54bebebeb440bada67c214286500c130080
Glenn Jackman <glenn.jackman@gmail.com>
2020-02-15 12:00:58 -0500

    code review for palindrome-products

--- exercises/palindrome-products/example.tcl
+++ exercises/palindrome-products/example.tcl
@@ -1,5 +1,5 @@
-# The simple nested loops solution is very slow.
-# This algorithm lifted from
+# The simple nested loops solution is very slow. The algorithm is
+# lifted from @ILoveMuffins's Python solution to the same problem:
 # https://exercism.io/tracks/python/exercises/palindrome-products/solutions/d68cab86cad94d4d821f26da44bb0722
 
 namespace eval palindromeProducts {
@@ -19,23 +19,20 @@ namespace eval palindromeProducts {
             error "min must be <= max"
         }
 
-        if {$step == 1} {
-            # smallest: start looping from the least product
-            set start [expr {$min ** 2}]
-            set end   [expr {$max ** 2}]
-            set loopCond {{i end} {expr {$i <= $end}}}
-        } else {
-            # largest: start looping from the greatest product
-            set start [expr {$max ** 2}]
-            set end   [expr {$min ** 2}]
-            set loopCond {{i end} {expr {$i >= $end}}}
-        }
-
         set prod -1
         set factors {}
 
-        set i $start
-        while {[apply $loopCond $i $end]} {
+        set minProduct [expr {$min ** 2}]
+        set maxProduct [expr {$max ** 2}]
+        if {$step > 0} {
+            # smallest: start looping from the least product
+            set range [list $minProduct $maxProduct $step]
+        } else {
+            # largest: start looping from the greatest product
+            set range [list $maxProduct $minProduct $step]
+        }
+
+        foreachInRange i $range {
             if {[isPalindrome $i]} {
                 set factors [factors $i $min $max $step]
                 if {[llength $factors] > 0} {
@@ -44,33 +41,56 @@ namespace eval palindromeProducts {
                     break
                 }
             }
-            incr i $step
         }
 
         return [list $prod $factors]
     }
 
     proc factors {n min max step} {
-        if {$step == 1} {
-            set i $min
-            set cond {{i n} {expr {$i**2 <= $n}}}
-        } else {
-            set i $max
-            set cond {{i n} {expr {$i**2 >= $n}}}
-        }
         set factors {}
+        set start [expr {$step > 0 ? $min : $max}]
+        set end   [expr {sqrt($n)}]
 
-        while {[apply $cond $i $n]} {
+        foreachInRange i [list $start $end $step] {
             set j [expr {$n / $i}]
             if {$i * $j == $n && $min <= $j && $j <= $max} {
                 lappend factors [lsort -integer [list $i $j]]
             }
-            incr i $step
         }
         return $factors
     }
 
     proc isPalindrome {n} {
         expr {$n eq [string reverse $n]}
+    }
+}
+
+
+# iterate over a closed range
+#
+# The `range` argument is a 3-element list:
+#     { startValue endValue step }
+#
+# example:
+#     foreachInRange i {1 10 2} {puts $i}
+#
+proc foreachInRange {loopVarname range script} {
+    lassign $range fromVal toVal step
+    if {
+            $step == 0
+         || ($step > 0 && $fromVal > $toVal)
+         || ($step < 0 && $fromVal < $toVal)
+    } {
+        error "infinite loop: start=$fromVal, end=$toVal, step=$step"
+    }
+
+    upvar 1 $loopVarname loopVar
+    set loopVar $fromVal
+    while {
+            ($step > 0 && $loopVar <= $toVal)
+         || ($step < 0 && $loopVar >= $toVal)
+    } {
+        uplevel 1 $script
+        incr loopVar $step
     }
 }
