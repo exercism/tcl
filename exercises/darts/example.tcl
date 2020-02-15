3646b99bf727ab3cc7d4645f9f0a46557e4fafdf
Glenn Jackman <glenn.jackman@gmail.com>
2020-02-15 12:00:46 -0500

    code review for darts

--- exercises/darts/example.tcl
+++ exercises/darts/example.tcl
@@ -1,7 +1,6 @@
 proc score {x y} {
-    set distance [::tcl::mathfunc::hypot $x $y]
-    if {$distance > 10} then {return 0}
-    if {$distance >  5} then {return 1}
-    if {$distance >  1} then {return 5}
-    return 10
+    set distance [expr {hypot($x, $y)}]
+    if {$distance <=  1} then {return 10}
+    if {$distance <=  5} then {return 5}
+    if {$distance <= 10} then {return 1} else {return 0}
 }
