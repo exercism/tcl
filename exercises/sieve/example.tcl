a90a421747179484b90db812fee6746efd2efde6
Glenn Jackman <glenn.jackman@gmail.com>
2020-02-15 12:01:13 -0500

    code review for sieve

--- exercises/sieve/example.tcl
+++ exercises/sieve/example.tcl
@@ -1,19 +1,18 @@
 proc primes {limit} {
+    # mark all candidates as prime
     set flags [lrepeat [expr {1 + $limit}] true]
 
-    set removeMultiples {{p limit} {
-        upvar 1 flags flags
+    # mark multiples of candidates as not prime
+    for {set p 2} {$p <= sqrt($limit)} {incr p} {
         if {[lindex $flags $p]} {
-            for {set m [expr {$p * $p}]} {$m <= $limit} {incr m $p} {
+            set step [expr {$p == 2 ? 2 : $p * 2}]
+            for {set m [expr {$p * $p}]} {$m <= $limit} {incr m $step} {
                 lset flags $m false
             }
         }
-    }}
-
-    for {set prime 2} {$prime <= int(sqrt($limit))} {incr prime} {
-        apply $removeMultiples $prime $limit
     }
 
+    # extract the remaining candidates
     set primes [list]
     for {set i 2} {$i <= $limit} {incr i} {
         if {[lindex $flags $i]} {
