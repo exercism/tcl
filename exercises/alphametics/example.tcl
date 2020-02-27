d12b39a9a0d066ad655f5544c9fcc04c0ed65ce6
Glenn Jackman <glenn.jackman@gmail.com>
2020-02-27 18:03:14 -0500

    remofe timings

--- exercises/alphametics/example.tcl
+++ exercises/alphametics/example.tcl
@@ -44,16 +44,3 @@ namespace eval alphametics {
 }
 
 namespace import alphametics::solve
-
-# timings for a test run:
-#   alphametics-1:      3543 microseconds per iteration
-#   alphametics-2:       364 microseconds per iteration
-#   alphametics-3:     27510 microseconds per iteration
-#   alphametics-4:      4987 microseconds per iteration
-#   alphametics-5:     24573 microseconds per iteration
-#   alphametics-6:    301756 microseconds per iteration
-#   alphametics-6:   2220392 microseconds per iteration
-#   alphametics-8:   9862008 microseconds per iteration
-#   alphametics-9:  24080449 microseconds per iteration
-#   alphametics-10: 28920954 microseconds per iteration
-#
