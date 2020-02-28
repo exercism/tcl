8e3fb6f052fc40b19e9fb7d3b2bde98ca815340c
Glenn Jackman <glenn.jackman@gmail.com>
2020-02-27 21:42:11 -0500

    Create exercise: strain

---
+++ exercises/strain/example.tcl
@@ -0,0 +1,16 @@
+proc keep {varname list condition {keep true}} {
+    upvar 1 $varname element
+    set kept [list]
+    foreach element $list {
+        if {!![uplevel 1 $condition] == !!$keep} {
+            lappend kept $element
+        }
+    }
+    return $kept
+}
+
+proc discard {varname list condition} {
+    # Use `tailcall` so that we're still 1 level below the caller:
+    # without it, this invocation of keep would put us 2 levels below.
+    tailcall keep $varname $list $condition false
+}
---
+++ exercises/strain/strain.tcl
@@ -0,0 +1,8 @@
+  
+proc keep {varname list condition} {
+    throw NOT_IMPLEMENTED "Implement this procedure."
+}
+
+proc discard {varname list condition} {
+    throw NOT_IMPLEMENTED "Implement this procedure."
+}
