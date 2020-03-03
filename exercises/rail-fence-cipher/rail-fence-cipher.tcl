b26e658647206efa2f2c8994458a35b3650ed156
Glenn Jackman <glenn.jackman@gmail.com>
2020-03-03 10:25:18 -0500

    Create exercise: rail-fence-cipher

---
+++ exercises/rail-fence-cipher/rail-fence-cipher.tcl
@@ -0,0 +1,12 @@
+namespace eval railFenceCipher {
+    namespace export encode decode
+    namespace ensemble create
+
+    proc encode {phrase num} {
+        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    }
+
+    proc decode {cipher num} {
+        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    }
+}
