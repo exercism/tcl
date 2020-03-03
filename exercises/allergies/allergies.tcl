e95ae8305fe0312a66a74c1341ae0de48522ee8d
Glenn Jackman <glenn.jackman@gmail.com>
2020-03-03 10:23:35 -0500

    Create exercise: allergies

---
+++ exercises/allergies/allergies.tcl
@@ -0,0 +1,7 @@
+proc allergicTo {allergen score} {
+    throw {NOT_IMPLEMENTED} "Implement this procedure."
+}
+
+proc listAllergies {score} {
+    throw {NOT_IMPLEMENTED} "Implement this procedure."
+}
