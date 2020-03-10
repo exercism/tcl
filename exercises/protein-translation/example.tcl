93704952d7b4421c59505af6bac698b91b9a4ca1
Glenn Jackman <glenn.jackman@gmail.com>
2020-03-10 12:44:05 -0400

    Create exercise: protein-translation

---
+++ exercises/protein-translation/example.tcl
@@ -0,0 +1,35 @@
+namespace eval proteinTranslation {
+    namespace export proteins
+
+    variable translation {
+        UGC Cysteine          UCA Serine
+        UGU Cysteine          UCC Serine
+        UUA Leucine           UCG Serine
+        UUG Leucine           UCU Serine
+        AUG Methionine        UGG Tryptophan
+        UUC Phenylalanine     UAC Tyrosine
+        UUU Phenylalanine     UAU Tyrosine
+
+        UAA STOP   UAG STOP   UGA STOP
+    }
+
+    proc proteins {strand} {
+        variable translation
+        set proteins {}
+        while {$strand ne ""} {
+            set codon  [string range $strand 0 2]
+            set strand [string range $strand 3 end]
+            if {![dict exists $translation $codon]} {
+                error "invalid codon $codon"
+            }
+            set protein [dict get $translation $codon]
+            if {$protein eq "STOP"} {
+                break
+            }
+            lappend proteins $protein
+        }
+        return $proteins
+    }
+}
+
+namespace import proteinTranslation::proteins
---
+++ exercises/protein-translation/protein-translation.tcl
@@ -0,0 +1,3 @@
+proc proteins {strand} {
+    throw {NOT_IMPLEMENTED} "Implement this procedure."
+}
