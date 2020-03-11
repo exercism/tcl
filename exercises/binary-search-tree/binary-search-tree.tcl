9b51136b5a77e8fc7cc64b41929175b92f482a60
Glenn Jackman <glenn.jackman@gmail.com>
2020-03-11 13:51:55 -0400

    Create exercise: binary-search-tree

---
+++ exercises/binary-search-tree/binary-search-tree.tcl
@@ -0,0 +1,11 @@
+oo::class create BinarySearchTree {
+    variable data
+    variable left
+    variable right
+
+    method insert {value} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    # other methods need to be implemented too...
+}
