08d8d05358858a5bc2db388f2620212d390198e5
Glenn Jackman <glenn.jackman@gmail.com>
2020-03-11 13:49:52 -0400

    Create exercise: atbash-cipher

---
+++ exercises/atbash-cipher/atbash-cipher.tcl
@@ -0,0 +1,12 @@
+namespace eval atbash {
+    namespace export encode decode
+    namespace ensemble create
+
+    proc encode {} {
+        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    }
+
+    proc decode {} {
+        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    }
+}
