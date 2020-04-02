35d5185d169e08417ebaa5411fcdd77d660138d0
Glenn Jackman <glenn.jackman@gmail.com>
2020-04-02 18:59:39 -0400

    Create exercise: affine-cipher

---
+++ exercises/affine-cipher/affine-cipher.tcl
@@ -0,0 +1,7 @@
+proc affine_encode {phrase a b} {
+    throw NOT_IMPLEMENTED "Implement this procedure."
+}
+
+proc affine_decode {phrase a b} {
+    throw NOT_IMPLEMENTED "Implement this procedure."
+}
