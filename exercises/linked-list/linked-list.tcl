a966c4b72a45a2ce54d21635beb7e25e128c84bc
Glenn Jackman <glenn.jackman@gmail.com>
2020-04-09 08:51:26 -0400

    Create exercise: linked-list

---
+++ exercises/linked-list/linked-list.tcl
@@ -0,0 +1,23 @@
+oo::class create LinkedList {
+    constructor {} {
+        throw {NOT_IMPLEMENTED} "Implement this class."
+    }
+
+    method push {data} {
+    }
+
+    method unshift {data} {
+    }
+
+    method pop {} {
+    }
+
+    method shift {} {
+    }
+
+    method for {varname script} {
+    }
+
+    method length {} {
+    }
+}
