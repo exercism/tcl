6aa6fbcb94586a99d219b2ed08e248e4c9b80368
Glenn Jackman <glenn.jackman@gmail.com>
2020-04-17 15:26:31 -0400

    Create exercise: bowling

---
+++ exercises/bowling/bowling.tcl
@@ -0,0 +1,13 @@
+oo::class create Bowling {
+    constructor {} {
+        throw {NOT_IMPLEMENTED} "Implement this class."
+    }
+
+    method roll {pins} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method score {} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+}
