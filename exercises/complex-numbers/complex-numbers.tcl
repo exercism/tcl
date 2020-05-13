d86f18d5cce8ff52e2eb9eb7c88c09f72f66567f
Glenn Jackman <glenn.jackman@gmail.com>
2020-05-13 00:09:12 -0400

    Create exercise: complex-numbers

---
+++ exercises/complex-numbers/complex-numbers.tcl
@@ -0,0 +1,45 @@
+oo::class create ComplexNumber {
+    constructor {real imag} {
+        throw {NOT_IMPLEMENTED} "Implement this class."
+    }
+
+    method real {} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method imag {} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method toList {} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method add {other} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method sub {other} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method mul {other} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method div {other} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method abs {} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method conj {} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method exp {} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+}
