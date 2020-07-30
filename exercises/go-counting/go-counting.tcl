ca30d407b3be7311d1e0696793048766adb5584f
Glenn Jackman <glenn.jackman@gmail.com>
2020-07-30 18:23:54 -0400

    Create exercise: go-counting

---
+++ exercises/go-counting/go-counting.tcl
@@ -0,0 +1,13 @@
+oo::class create GoBoard {
+    constructor {input} {
+        throw {NOT_IMPLEMENTED} "Implement this class."
+    }
+
+    method territory {intersection} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+
+    method territories {intersection} {
+        throw {NOT_IMPLEMENTED} "Implement this method."
+    }
+}
