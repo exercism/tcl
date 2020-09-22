f2494044bc32fdd0b25072203bb79df66842d084
Glenn Jackman <glenn.jackman@gmail.com>
2020-09-22 13:57:47 -0400

    fix the stub file

--- exercises/diffie-hellman/diffie-hellman.tcl
+++ exercises/diffie-hellman/diffie-hellman.tcl
@@ -1,14 +1,13 @@
-namespace eval diffieHellman {}
+namespace eval diffieHellman {
+    proc privateKey {p} {
+        throw {NOT_IMPLEMENTED} "Implement this procedure"
+    }
 
-proc diffieHellman::privateKey {p} {
-    # a random number in the range [2, p)
-    return [expr {2 + int(($p - 2) * rand())}]
-}
+    proc publicKey {p g private} {
+        throw {NOT_IMPLEMENTED} "Implement this procedure"
+    }
 
-proc diffieHellman::publicKey {p g private} {
-    return [expr {($g ** $private) % $p}]
-}
-
-proc diffieHellman::secret {p public private} {
-    return [publicKey $p $public $private]
+    proc secret {p public private} {
+        throw {NOT_IMPLEMENTED} "Implement this procedure"
+    }
 }
