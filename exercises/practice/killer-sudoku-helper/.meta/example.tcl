8ddd46a014752af35092e9a0ab7d456d7255130c
Glenn Jackman <glenn.jackman@gmail.com>
2023-01-06 13:20:01 -0500

    don't hardcode the proc name for the recursive call

--- exercises/practice/killer-sudoku-helper/.meta/example.tcl
+++ exercises/practice/killer-sudoku-helper/.meta/example.tcl
@@ -1,15 +1,14 @@
 proc combinations {sum size exclude} {
+    set procname [lindex [info level 0] 0]
     if {$size == 1} {
         if {1 <= $sum && $sum <= 9 && $sum ni $exclude} {
             return $sum
-        } else {
-            return
         }
     } else {
         set results [dict create]
         for {set n 1} {$n <= 9} {incr n} {
             if {$n ni $exclude} {
-                foreach comb [combinations \
+                foreach comb [$procname \
                                 [expr {$sum - $n}] \
                                 [expr {$size - 1}] \
                                 [concat $exclude $n]] {
