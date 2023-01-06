9cb2b0a2c525c2b54f284e5fef5a12bdc146cfc3
Glenn Jackman <glenn.jackman@gmail.com>
2023-01-06 13:21:04 -0500

    remove solution from stub

--- exercises/practice/killer-sudoku-helper/killer-sudoku-helper.tcl
+++ exercises/practice/killer-sudoku-helper/killer-sudoku-helper.tcl
@@ -1,22 +1,3 @@
 proc combinations {sum size exclude} {
-    if {$size == 1} {
-        if {1 <= $sum && $sum <= 9 && $sum ni $exclude} {
-            return $sum
-        } else {
-            return
-        }
-    } else {
-        set results [dict create]
-        for {set n 1} {$n <= 9} {incr n} {
-            if {$n ni $exclude} {
-                foreach comb [combinations \
-                                [expr {$sum - $n}] \
-                                [expr {$size - 1}] \
-                                [concat $exclude $n]] {
-                    dict set results [lsort [concat $comb $n]] 1
-                }
-            }
-        }
-        return [dict keys $results]
-    }
+    throw {NOT_IMPLEMENTED} "Implement this procedure."
 }
