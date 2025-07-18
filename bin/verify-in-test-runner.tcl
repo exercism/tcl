244ec98ffef0c7e142d3fd5686ef8cc72a36bba2
Glenn Jackman <glenn.jackman@gmail.com>
2025-07-18 14:34:40 -0400

    Various fixes and tweaks to get the PR workflow testing only the submitted exercises.

--- bin/verify-in-test-runner.tcl
+++ bin/verify-in-test-runner.tcl
@@ -5,28 +5,15 @@
 
 package require json
 
-# Synopsis:
-# Verify that each exercise's example/exemplar solution passes the tests
-# using the track's test runner Docker image.
-# You can either verify all exercises or a single exercise.
-
-# Example: verify all exercises in Docker
-# bin/verify-exercises-in-docker.tcl
-
-# Example: verify specific exercises in Docker
-# bin/verify-exercises-in-docker.tcl two-fer leap reverse-string
-
-
-proc run_tests {directory} {
-    set dir [file dirname $directory]
-    set slug [file tail $directory]
+proc run_tests {dir} {
+    set slug [file tail $dir]
 
     puts -nonewline "Verifying $slug exercise... "
 
     cd $dir
 
     set f [open ./.meta/config.json r]
-    set config [json::json2dict [read $f]]
+    set config [::json::json2dict [read $f]]
     close $f
 
     # if we get concept exercises, or if we get an exercise
@@ -34,12 +21,12 @@ proc run_tests {directory} {
     set example [dict get $config files example]
     set solution [dict get $config files solution]
 
-    file copy --force $example $solution
+    file copy -force $example $solution
 
     exec /opt/test-runner/bin/run.tcl $slug [pwd] [pwd]
 
     set f [open ./results.json r]
-    set result [json::json2dict [read $f]]
+    set result [::json::json2dict [read $f]]
     close $f
 
     puts [dict get $result status]
@@ -50,23 +37,38 @@ proc run_tests {directory} {
     }
 }
 
-proc verify_exercises {slugs} {
-    if {[llength $slugs] == 0} {
-        set dirs [glob ./exercises/practice/*]
-    } else {
-        set dirs [lmap slug $slugs {
-            set dir "./exercises/practice/$slug"
-            expr {[file isdirectory $dir] ? $dir : [continue]}
-        }
-    }
+############################################################
+proc get_dirs {pr_number} {
+    set url "https://api.github.com/repos/exercism/tcl/pulls/$pr_number/files"
+    set json_string [exec wget --quiet --output-document=- $url]
+    set file_info [::json::json2dict $json_string]
 
+    set regex {(exercises/(?:concept|practice)/[^/]+)/.*(?:tcl|test)$}
+    set exercise_dirs [lmap item $file_info {
+        set filename [dict get $item filename]
+        if {[regexp $regex $filename -> dir]} {
+            string cat $dir
+        } else continue
+    }]
+
+    lsort -unique $exercise_dirs
+}
+
+############################################################
+proc verify_exercises {dirs} {
     if {[llength $dirs] == 0} {
-        error "no matching exercises found!"
+        set dirs [glob exercises/practice/*]
     }
-
     foreach exercise_dir $dirs {
         run_tests $exercise_dir
     }
 }
 
-verify_exercises $argv
+############################################################
+lassign $argv pr_number
+
+if {$pr_number eq ""} {
+    verify_exercises {}
+} else {
+    verify_exercises [get_dirs $pr_number]
+}
