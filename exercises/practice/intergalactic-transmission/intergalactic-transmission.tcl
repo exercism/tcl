b3950a1ec229accfb68747062ad28993091912a0
Glenn Jackman <glenn.jackman@gmail.com>
2025-08-19 13:29:32 -0400

    Add intergalactic-transmission exercise (#397)

---
+++ exercises/practice/intergalactic-transmission/intergalactic-transmission.tcl
@@ -0,0 +1,7 @@
+proc transmitSequence {message} {
+    throw {NOT_IMPLEMENTED} "Implement this procedure."
+}
+
+proc decodeMessage {transmission} {
+    throw {NOT_IMPLEMENTED} "Implement this procedure."
+}
